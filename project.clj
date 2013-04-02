cc9f3b64350180a7ff56d78b9e7c32fd1382c3c1
narkisr <narkisr@gmail.com>
2013-04-02 10:29:31 +0300

    initial import

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject com.narkisr/gelfino-client "0.4.0"
+  :description ""
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.taoensso/timbre "1.5.2"]
+                 [cheshire "5.0.2"]]
+
+  :profiles {:dev {:dependencies [ ]}}
+
+)
