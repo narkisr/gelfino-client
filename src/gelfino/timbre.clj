e9b77e70e67c0d760605242a7aadf0b530dbdd9c
narkisr <narkisr@gmail.com>
2013-06-27 17:08:23 +0300

    adding tid

--- src/gelfino/timbre.clj
+++ src/gelfino/timbre.clj
@@ -6,11 +6,25 @@
     [gelfino.client :only (connect send-> client-socket) ]))
 
 
+(def ^{:doc "A transaction id that can be used to trace back a logical log flow in central logging 
+             systems like Kibana/Graylog2" 
+       :dynamic true :private true} tid nil)
+
+(defmacro set-tid 
+   "Sets tid for all current thread logs" 
+   [tid* body]
+  `(binding [tid ~tid*] ~body))
+
 (def ^{:doc "Timbre level to Gelf log level"}
   levels {:trace 1 :debug 7 :info 6 :warn 5 :error 4 :fatal 3 :unknown 1})
 
 (def ^{:doc "Logging machine hostname"}
   hostname (.getHostName (java.net.InetAddress/getLocalHost)))
+
+(defn append-tid 
+   "Appends transaction id number if exists" 
+   [m]
+  (if tid (assoc m :_tid tid) m))
 
 (def ^{:doc "A Gelf append for Timbre"}
   gelf-appender
@@ -24,6 +38,5 @@
      (when-not @client-socket (connect))
      (let [message* (if (seq more) (str message " " (join " " more)) message)]
        (send-> (get-in ap-config [:gelf :host]) 
-          {:short_message message* :message message* :level (levels level) 
-           :facility "gelfino" :host hostname}))
-     )})
+          (append-tid {:short_message message* :message message* :level (levels level) 
+                       :facility "gelfino" :host hostname}))))})
