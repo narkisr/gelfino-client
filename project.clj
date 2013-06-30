b003e30fb29d98c7389a5d65d80984f27e397df7
Ronen Narkis <narkisr@gmail.com>
2013-06-30 05:26:54 +0300

    version bump

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.narkisr/gelfino-client "0.4.1"
+(defproject com.narkisr/gelfino-client "0.4.2"
   :description "A Gelf logging client including a timbre adapter"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.taoensso/timbre "1.5.2"]
                  [cheshire "5.0.2"]]
   
-  :plugins  [[codox "0.6.4"]]
+  :plugins  [[lein-tag "0.1.0"] [codox "0.6.4"]]
 
   :profiles {:dev {:dependencies [ ]}}
 
