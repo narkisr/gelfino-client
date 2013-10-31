31c8341bfc4833e72195aa64ceb5add186f351b0
Ronen Narkis <narkisr@gmail.com>
2013-10-31 18:07:40 +0200

    adding throwable to error field and cause

--- src/gelfino/timbre.clj
+++ src/gelfino/timbre.clj
@@ -1,9 +1,9 @@
 (ns gelfino.timbre
   "A Gelf based Timbre appender" 
-  (:require [taoensso.timbre :as timbre] )
+  (:require [taoensso.timbre :as t])
   (:use 
     [clojure.string :only (join)] 
-    [gelfino.client :only (connect send-> client-socket) ]))
+    [gelfino.client :only (send-> lazy-connect) ]))
 
 
 (def ^{:doc "A transaction id that can be used to trace back a logical log flow in central logging 
@@ -22,8 +22,8 @@
    tid
   )
 
-(def ^{:doc "Timbre level to Gelf log level"}
-  levels {:trace 1 :debug 7 :info 6 :warn 5 :error 4 :fatal 3 :unknown 1})
+(def ^{:doc "Timbre level to Gelf log level see http://bit.ly/154nolw note that trace isn't defined"}
+  levels {:trace -1 :debug 0 :info 1 :warn 2 :error 3 :fatal 4 :unknown 5})
 
 (def ^{:doc "Logging machine hostname"}
   hostname (.getHostName (java.net.InetAddress/getLocalHost)))
@@ -33,17 +33,24 @@
    [m]
   (if tid (assoc m :_tid tid) m))
 
+(defn format-message 
+  "formats message for sending" 
+  [{:keys [config level prefix throwable message] :as args}]
+    (let [res  {:short_message message :full_message message 
+                :level (levels level) :facility "gelfino" :host hostname}]
+      (if throwable 
+        (merge res {:error (t/stacktrace throwable) :message (.getMessage throwable)})
+        res) 
+      ))
+
 (def ^{:doc "A Gelf append for Timbre"}
   gelf-appender
   {:doc       "A gelfino based appender"
    :min-level :debug
    :enabled?  true
    :async?    false
-   :max-message-per-msecs nil 
+   :limit-per-msecs nil 
    :fn 
-   (fn [{:keys [ap-config level prefix message more] :as args}]
-     (when-not @client-socket (connect))
-     (let [message* (if (seq more) (str message " " (join " " more)) message)]
-       (send-> (get-in ap-config [:gelf :host]) 
-          (append-tid {:short_message message* :message message* :level (levels level) 
-                       :facility "gelfino" :host hostname}))))})
+   (fn [{:keys [ap-config] :as args}]
+     (lazy-connect)
+     (send-> (get-in ap-config [:gelf :host]) (append-tid (format-message args))))})
