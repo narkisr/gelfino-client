5ff311b90c8aad86c79e285378ea3759b35dde92
Ronen Narkis <narkisr@gmail.com>
2013-10-31 16:55:52 +0200

    moving to timbre 2.6.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject com.narkisr/gelfino-client "0.5.0"
+(defproject com.narkisr/gelfino-client "0.6.0"
   :description "A Gelf logging client including a timbre adapter"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.taoensso/timbre "2.4.1"]
+                 [com.taoensso/timbre "2.6.3"]
                  [cheshire "5.0.2"]]
   
   :plugins  [[lein-tag "0.1.0"] [codox "0.6.4"]]
