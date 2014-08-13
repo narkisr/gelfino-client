aec8ad27885e0e3d4baf5862d84b6915c70b08c6
narkisr <narkisr@gmail.com>
2014-08-14 01:12:49 +0300

    adding midje and testing

--- project.clj
+++ project.clj
@@ -1,8 +1,18 @@
-(defproject com.narkisr/gelfino-client "0.6.0"
+(defproject com.narkisr/gelfino-client "0.7.0"
   :description "A Gelf logging client including a timbre adapter"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.taoensso/timbre "2.6.3"]
-                 [cheshire "5.0.2"]]
+  :dependencies [
+        [org.clojure/clojure "1.5.1"]
+        [com.taoensso/timbre "2.6.3"]
+        [cheshire "5.0.2"]]
+
+  :profiles {
+    :dev {
+      :dependencies [
+        [midje "1.5.1"]
+        [clj-http "1.0.0"]
+      ]        
+    }            
+  }
   
-  :plugins  [[lein-tag "0.1.0"] [codox "0.6.4"]]
+  :plugins  [[lein-tag "0.1.0"] [codox "0.6.4"] [lein-midje "3.0.0"]]
 )
